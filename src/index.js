c46fe47bba3113ece830c20607937fc19a257873
RickTurbo <73983715+RickTurbo@users.noreply.github.com>
2021-11-01 00:36:33 +0900

    initialcolorの変更

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <ChakraProvider>
-      <ColorModeScript initialColorMode="light"></ColorModeScript>
+      <ColorModeScript initialColorMode="dark"></ColorModeScript>
       <App />
     </ChakraProvider>
   </StrictMode>,
